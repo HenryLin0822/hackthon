2e7003497a3956712cbfc7c20f2994957ae26828
henry <henrylin.0822@gmail.com>
2024-08-05 07:33:20 +0800

    fine-tune

---
+++ fine-tune/main.py
@@ -0,0 +1,19 @@
+import openai
+
+api_key = ''
+
+client = openai.Client(api_key = api_key)
+
+with open('Daan_Park.json', 'rb') as f:
+    response = client.files.create(file=f, purpose='fine-tune')
+
+print(response)
+
+file_id = 'file-G2FJxSkWfvUuo5lGbVXPiORn'
+
+response = client.fine_tuning.jobs.create(
+    training_file=file_id,
+    model="gpt-2"
+    )
+
+print(response)
