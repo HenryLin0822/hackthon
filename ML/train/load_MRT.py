4d4272175853c8756bafdb86aa4ea87b850b1e54
Henry <henrylin.0822@gmail.com>
2024-08-10 18:24:48 +0800

    fixing MRT data

--- ML/train/load_MRT.py
+++ ML/train/load_MRT.py
@@ -22,18 +22,35 @@ def load_ods(filename):
 
 def combine_dataframes(df_1, df_2):
     # Ensure the same order of columns in both DataFrames
-    df_2 = df_2[df_1.columns]
+    df_2 = df_2.reindex(columns=df_1.columns, fill_value=pd.NA)
     # Concatenate the DataFrames row-wise
     combined_df = pd.concat([df_1, df_2], ignore_index=True)
+    
+    return combined_df
+def combine_mutiple_dataframes(dfs):
+    combined_df = None
+    for i in range(len(dfs)):
+        if i == 0:
+            combined_df = dfs[i]
+        else:
+            combined_df = combine_dataframes(combined_df, dfs[i])
     
     return combined_df
 # For testing purposes
 if __name__ == "__main__":
     df_1 = load_ods("../data/202401_cht.ods")[:31]
     df_2 = load_ods("../data/202402_cht.ods")[:28]
+    df_3 = load_ods("../data/202403_cht.ods")[:31]
+    df_4 = load_ods("../data/202404_cht.ods")[:30]
+    df_5 = load_ods("../data/202405_cht.ods")[:31]
+    df_6 = load_ods("../data/202406_cht.ods")[:30]
 
     #print('df_1:', df_1)
     #print('df_2:', df_2)
-    combined_df = combine_dataframes(df_1, df_2)
+    #print('df_3:', df_3)
+    #print('df_4:', df_4)
+    #print('df_5:', df_5)
+    print('df_6:', df_6)
+    combined_df = combine_mutiple_dataframes([df_1, df_2, df_3, df_4])
     for i in range((len(combined_df.columns))):
         print(combined_df['　　　　車站\n日期'][i])
