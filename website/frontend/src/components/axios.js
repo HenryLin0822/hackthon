32b9a2cfd189a5084db385c07277765e858373d0
trix0831 <joseph831hsu@gmail.com>
2024-09-07 21:02:28 +0800

    chatbot fix

--- website/frontend/src/components/axios.js
+++ website/frontend/src/components/axios.js
@@ -3,7 +3,7 @@ import _axios from "axios";
 const instance = _axios.create({
   baseURL:
     process.env.REACT_APP_API_URL || "https://cktfgsc-bank-z3zpvj7vba-de.a.run.app/api",
-  timeout: 2000,
+  timeout: 10000,
 });
 
 export default instance;
